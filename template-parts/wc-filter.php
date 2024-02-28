14c79c70ac74b2e900acb38196a4350a3fa740db
dhavalp-111 <dhaval.p@dolphinwebsolutions.com>
2024-02-28 18:55:06 +0530

    push woo-filter

---
+++ template-parts/wc-filter.php
@@ -0,0 +1,27 @@
+<div id="wk-custom-shop">
+    <?php echo 'helii';
+    // $args     = array(
+    //             'post_type' => 'product',
+
+    //     'status' => 'publish',
+    //     'limit'  => -1,
+    // )
+    // $products = wc_get_products( $args );
+
+    // if ( $products ) {
+    //     echo '<ul class="wk-product-list">';
+    //     foreach ( $products as $product ) {
+    //         echo '<li class="wk-product">';
+    //         echo '<h2>' . $product->get_name() . '</h2>';
+    //         echo '<p>' . $product->get_price_html() . '</p>';
+    //         // Add more product details here
+    //         echo '</li>';
+    //     }
+    //     echo '</ul>';
+    // } else {
+    //     echo 'No products found.';
+    // }
+    ?>
+    <h1>hello</h1>
+
+</div>  
